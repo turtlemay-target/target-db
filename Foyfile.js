7555e1b80cce9c85bc4aee7b96721583582ff7b5
Turtlemay <am@trtl.in>
2020-10-02 01:41:14 -0400

    Initial.

---
+++ Foyfile.js
@@ -0,0 +1,53 @@
+const { task, fs } = require('foy');
+const { Validator } = require('jsonschema');
+const manifestJson = require('./manifest.json');
+const schemaJson = require('target-db-schema/schema.json');
+
+const BUILD_DIR = `${__dirname}/build`;
+
+task('build', ['validate'], async (ctx) => {
+    await fs.outputJson(`${BUILD_DIR}/data.json`, buildData());
+});
+
+task('validate', async (ctx) => {
+    const validator = new Validator();
+    try {
+        const result = validator.validate(buildData(), schemaJson);
+        if (result.errors.length > 0) {
+            const [err] = result.errors;
+            throw new Error(`${err.property} ${err.message}`);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+task('clean', async (ctx) => {
+    await fs.rmrf(BUILD_DIR);
+});
+
+function buildData() {
+    return {
+        name: manifestJson.name,
+        version: manifestJson.version,
+        items: readData(),
+    };
+}
+
+/**
+ * @param {string} dataDir Read all files from this directory as JSON.
+ * @returns {Array} An array of item data objects.
+ */
+function readData(dataDir = `${__dirname}/data`) {
+    const fileNames = fs.readdirSync(dataDir);
+    const filePaths = fileNames.map(
+        fileName => `${dataDir}/${fileName}`,
+    );
+    const fileContentsArr = (
+        filePaths.map(v => fs.readFileSync(v, 'utf-8'))
+    );
+    const itemDataArrays = fileContentsArr.map(
+        fileContents => JSON.parse(fileContents)
+    );
+    return [].concat(...itemDataArrays);
+}
